c3426360b1b9f1526266aaa8ce5f567fdabc514d
FabulousStarfish <71825170+FabulousStarfish@users.noreply.github.com>
2021-03-04 09:41:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,22 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-NPLdWkbf/model.json',modelLoaded);
+}
+function modelLoaded(){
+    console.log("MODEL LOADED !")
 }
 function draw(){
     image(video,0,0,337.5,300);
+    classifier.classify(video,checkImages);
+}
+function checkImages(error, result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById("result").innerHTML=result[0].label;
+        document.getElementById("accuracy").innerHTML=result[0].confidence.toFixed(2);
+    }
 }
